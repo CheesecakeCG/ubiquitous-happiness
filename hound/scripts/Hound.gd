6e405e1dba865271efaca4f9c08156b654956a88
Chris D (CheesecakeCG) <cheesecakecgcontact@gmail.com>
2019-04-21 18:56:30 -0400

    New playermodel, Giving up on rigidbody

--- hound/scripts/Hound.gd
+++ hound/scripts/Hound.gd
@@ -1,65 +1,53 @@
 extends RigidBody
 
-export var pull_force : float = 500
-export var kick_force : float = 800
+export var kick_force : float = 500
 export var jump_force : float = 100
+export var stopping_force : float = 500
+export var stopping_turn : float = PI/2
 
-var turn : float = 0
-var kickNext = true
+var turn : float = 0 setget _set_turn
+
+var accel : float = 0
 
 func _ready():
 #	turn = angular_velocity.y
 	pass
 
+func _process(delta: float) -> void:
+	if accel > 100:
+		$"Mesh/Scene Root/AnimationPlayer".play("run")
+	else:
+		$"Mesh/Scene Root/AnimationPlayer".stop()
+
 func _input(event: InputEvent) -> void:
 	if is_network_master():
-		if (Input.is_action_pressed("player_forward")) :
-			rpc_unreliable("take_step")
+		if (Input.is_action_pressed("player_forward")):
+			player_forward()
 	if event is InputEventMouseMotion && Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		turn -= event.relative.x * .16 * .01
-#		turn = clamp(turn, -.3, .3)
-#		turn = clamp(turn, -PI/2, PI/2)
-		turn = wrapf(turn, -2*PI, 2*PI)
-#		$Mesh/DirectionIndicator.rotation.y = turn
+		rpc("_set_turn", turn - event.relative.x * .16 * .01)
 		$Mesh.rotation.y = turn
-		$Mesh.rotation.z = wrapf(-turn, -PI/12, PI/12)
-#		$DirectionIndicator.translation = Vector3(-turn, 0, -2.2)
+		$Mesh.rotation.z = turn / 2
+
+func player_forward():
+	rpc("_set_accel", kick_force)
+
+sync func _set_turn(t : float):
+	if abs(t) < .05:
+		t = 0
+	if abs(turn - t) < .01:
+		t = t - (sign(t) * stopping_force)
+	turn = clamp(t, -PI/12, PI/12)
+
+sync func _set_accel(a : float):
+	if abs(a) < 50:
+		a = 0
+	accel = clamp(a, -500, 500)
 
 func _physics_process(delta: float) -> void:
-
-	pass
-
-sync func run(force : Vector3):
-	pass
-
-sync func take_step() -> void:
-	if (translation.y < .1 && translation.y >= 0):
-		print("Can Move")
-		var direction = Vector3()
-		if !kickNext:
-			print("Pull")
-			direction += Vector3(0, 0, pull_force)
-			kickNext = true
-		else:
-			print("Kick")
-			direction += Vector3(0, 0, kick_force)
-			kickNext = false
-
-#		apply_torque_impulse(Vector3(0, turn, 0).rotated(transform.basis.y, wrapf(rotation.y, -PI, PI)))
-#		angular_velocity += Vector3(0, turn, 0)
-#		turn = 0
-#		angular_velocity = angular_velocity/4
-
-#		var t = Vector3(turn, 0, -2.2)
-#		turn += rotation.y
-
-		turn =+ rotation.y
-#		direction = direction.rotated(transform.basis.y, wrapf(rotation.y + turn, -PI, PI)) # Rotated only likes the least coterminal for some reason.
-		direction = direction.rotated(transform.basis.y, turn) # Rotated only likes the least coterminal for some reason.
-
-		apply_central_impulse(direction)
-		print(direction)
+	angular_velocity.y += turn * 40 * delta
+	apply_central_impulse(Vector3(0, 0, accel).rotated(transform.basis.y, rotation.y))
+	rpc("_set_accel", accel - (sign(accel) * stopping_force * delta))#	rpc("_set_turn", turn - (sign(turn) * stopping_turn * delta))
+	angular_velocity.y = clamp(angular_velocity.y, -10, 10)
 
 
-		$Timer.start()
-	pass
+
