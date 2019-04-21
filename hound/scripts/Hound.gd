8fa6988438db62b9d8c4f7dc58bded0853f8ec1f
Chris D (CheesecakeCG) <cheesecakecgcontact@gmail.com>
2019-04-21 17:35:41 -0400

    Movement sort of work; Will get rewritten.

--- hound/scripts/Hound.gd
+++ hound/scripts/Hound.gd
@@ -1,8 +1,8 @@
 extends RigidBody
 
-export var pull_force : float = 30000
-export var kick_force : float = 70000
-export var jump_force : float = 10000
+export var pull_force : float = 500
+export var kick_force : float = 800
+export var jump_force : float = 100
 
 var turn : float = 0
 var kickNext = true
@@ -16,32 +16,50 @@ func _input(event: InputEvent) -> void:
 		if (Input.is_action_pressed("player_forward")) :
 			rpc_unreliable("take_step")
 	if event is InputEventMouseMotion && Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		turn -= event.relative.x * .16 * .02
-		turn = clamp(turn, -PI/4, PI/4)
-		$DirectionIndicator.rotation.y = turn
-
+		turn -= event.relative.x * .16 * .01
+#		turn = clamp(turn, -.3, .3)
+#		turn = clamp(turn, -PI/2, PI/2)
+		turn = wrapf(turn, -2*PI, 2*PI)
+#		$Mesh/DirectionIndicator.rotation.y = turn
+		$Mesh.rotation.y = turn
+		$Mesh.rotation.z = wrapf(-turn, -PI/12, PI/12)
+#		$DirectionIndicator.translation = Vector3(-turn, 0, -2.2)
 
 func _physics_process(delta: float) -> void:
 
 	pass
 
+sync func run(force : Vector3):
+	pass
+
 sync func take_step() -> void:
-	if ($Timer.time_left <= 0 && translation.y < .05 && translation.y >= 0):
+	if (translation.y < .1 && translation.y >= 0):
 		print("Can Move")
 		var direction = Vector3()
 		if !kickNext:
 			print("Pull")
-			direction += Vector3(0, jump_force, pull_force)
+			direction += Vector3(0, 0, pull_force)
 			kickNext = true
 		else:
 			print("Kick")
-			direction += Vector3(0, jump_force, kick_force)
+			direction += Vector3(0, 0, kick_force)
 			kickNext = false
 
-		apply_torque_impulse(Vector3(0, turn, 0))
+#		apply_torque_impulse(Vector3(0, turn, 0).rotated(transform.basis.y, wrapf(rotation.y, -PI, PI)))
+#		angular_velocity += Vector3(0, turn, 0)
+#		turn = 0
+#		angular_velocity = angular_velocity/4
 
-		direction = direction.rotated(transform.basis.y, wrapf(rotation.y, -PI, PI)) # Rotated only likes the least coterminal for some reason.
+#		var t = Vector3(turn, 0, -2.2)
+#		turn += rotation.y
+
+		turn =+ rotation.y
+#		direction = direction.rotated(transform.basis.y, wrapf(rotation.y + turn, -PI, PI)) # Rotated only likes the least coterminal for some reason.
+		direction = direction.rotated(transform.basis.y, turn) # Rotated only likes the least coterminal for some reason.
+
 		apply_central_impulse(direction)
+		print(direction)
+
 
 		$Timer.start()
 	pass
