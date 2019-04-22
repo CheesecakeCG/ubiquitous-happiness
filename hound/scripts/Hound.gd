9d296d2177a1d429342445d1be880c49ffad29bc
Chris D (CheesecakeCG) <cheesecakecgcontact@gmail.com>
2019-04-21 23:16:10 -0400

    Basic jumping added

--- hound/scripts/Hound.gd
+++ hound/scripts/Hound.gd
@@ -1,13 +1,12 @@
 extends VehicleBody
 
 export var kick_force : float = 600
-export var jump_force : float = 100
+export var jump_force : float = 10
 export var stopping_force : float = 500
 export var stopping_turn : float = PI/2
 
 
 func _ready():
-#	turn = angular_velocity.y
 	pass
 
 func _process(delta: float) -> void:
@@ -15,16 +14,18 @@ func _process(delta: float) -> void:
 		if (Input.is_action_pressed("player_skid")):
 			rpc("_brake", kick_force / 25)
 		else:
+			if (Input.is_action_just_pressed("player_jump")):
+#				TODO: Check if on ground!
+				apply_central_impulse(Vector3(0, jump_force * mass, 0))
 			if (Input.is_action_pressed("player_forward")):
 				rpc("_gas", kick_force)
-				print("Gas gas gas!")
 			else:
 				if (Input.is_action_pressed("player_backwards")):
 					rpc("_gas", -kick_force)
 				else:
 					rpc("_brake", kick_force / 2)
 
-	if abs(engine_force) > 100:
+	if abs(linear_velocity.length()) > 100:
 		$"Mesh/Scene Root/AnimationPlayer".play("run")
 	else:
 		$"Mesh/Scene Root/AnimationPlayer".stop()
@@ -38,7 +39,6 @@ func _input(event: InputEvent) -> void:
 	if is_network_master():
 		if event is InputEventMouseMotion && Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 			rpc("_steer", steering - event.relative.x * .16 * .005)
-#			$Mesh.rotation.y = steering
 			$Mesh.rotation.z = -steering / 2
 			$DirectionIndicator.rotation.y = steering
 
